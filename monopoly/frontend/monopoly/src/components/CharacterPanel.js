7ab99f960392b10ec3e357c689aa569e3c40497d
hitup123 <118286747+hitup123@users.noreply.github.com>
2024-07-26 10:34:59 +0530

    Revert "Revert "Update 25-07-2024 test""

--- monopoly/frontend/monopoly/src/components/CharacterPanel.js
+++ monopoly/frontend/monopoly/src/components/CharacterPanel.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CharacterPanel({imageURL})
-{
+function charselect(event) {
+    const obj = event.currentTarget;
+    const isPressed = obj.getAttribute('data-pressed') === 'true';
+    const statusled = obj.children[1];
+
+    if (isPressed) {
+        // Unpressed state
+        obj.style.border = '5px outset';
+        obj.style.borderRadius = '100%';
+        obj.style.boxShadow = '';
+        obj.style.transform = '';
+        obj.setAttribute('data-pressed', 'false');
+        statusled.style.backgroundColor = 'white';
+    } else {
+        // Pressed state
+        obj.style.border = '5px inset';
+        obj.style.borderRadius = '100%';
+        obj.style.boxShadow = 'inset 0 0 10px rgba(0, 0, 0, 0.5)';
+        obj.style.transform = 'translateY(2px)';
+        obj.setAttribute('data-pressed', 'true');
+        statusled.style.backgroundColor = 'green';
+
+    }
+}
+
+function CharacterPanel({ imageURL }) {
     const panelcss = {
-        backgroundColor: 'rgb(255, 255, 255, 0.5)',
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
         height: '95%',
         width: '110px',
         borderRadius: '100%',
         padding: '5px',
         margin: '5px',
-        border: '5px outset '
+        border: '5px outset',
+        transition: 'all 0.1s ease'
     };
 
     const image = {
@@ -17,14 +42,26 @@ function CharacterPanel({imageURL})
         width: 'auto',
         position: 'relative',
         left: '50%',
-        translate: '-50% 0%'
+        transform: 'translateX(-50%)'
+    };
+
+    const indicator = {
+        height: '5px',
+        width: '5px',
+        border: 'outset grey 1px',
+        borderRadius: '100%',
+        position: 'relative',
+        left: '50%',
+        top: '-120%',
+        translate: '-50%'
     }
 
     return (
-        <div style={panelcss}>
+        <div style={panelcss} onClick={charselect} data-pressed="false">
             <img alt="image" src={imageURL} style={image} />
+            <div style={indicator}></div>
         </div>
     );
 }
 
-export default CharacterPanel
+export default CharacterPanel;
