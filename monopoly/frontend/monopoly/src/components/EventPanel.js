dc82eb2e6df2f42dc038eeb447989d1dd4ae190f
Zeta54633 <yashmathur.693@gmail.com>
2024-09-03 23:39:06 +0530

    rough sketch of chance

--- monopoly/frontend/monopoly/src/components/EventPanel.js
+++ monopoly/frontend/monopoly/src/components/EventPanel.js
@@ -1,35 +1,22 @@
-import React, { useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import './EventPanel.css';
 
 import CommunityEvent from './Events/CommunityEvent';
 import ChanceEvent from './Events/ChanceEvent';
 
-function EventPanel() {
-    const iframeRef = useRef(null);
+function EventPanel({ eventType }) { // eventType is received as a prop
 
-    useEffect(() => {
-        const iframeDoc = iframeRef.current.contentDocument || iframeRef.current.contentWindow.document;
-        iframeDoc.open();
-        iframeDoc.write('<!DOCTYPE html><html><head><style></style></head><body><div id="iframe-root"></div></body></html>');
-        iframeDoc.close();
+    let content;
 
-        // Here you can render any React component inside the iframe
-        var eventType;
-
-        if(eventType)
-        {
-            ReactDOM.render(<CommunityEvent />, iframeDoc.getElementById('iframe-root'));
-        }
-        else
-        {
-            ReactDOM.render(<ChanceEvent />, iframeDoc.getElementById('iframe-root'));
-        }
-    }, []);
+    if (eventType) {
+        content = <CommunityEvent />;
+    } else {
+        content = <ChanceEvent />;
+    }
 
     return (
         <div id="eventpanel">
-            <iframe ref={iframeRef} title="Event Panel" width="100%" height="400px" frameBorder="0"></iframe>
+            {content}
         </div>
     );
 }
--- monopoly/frontend/monopoly/src/components/Events/ChanceEvent.js
+++ monopoly/frontend/monopoly/src/components/Events/ChanceEvent.js
@@ -2,9 +2,24 @@ import React from "react";
 
 function ChanceEvent()
 {
-    return (
-        <h1>This is Chance Chest</h1>
-    )
+    const card = {
+        backgroundColor: "rgb(0, 0, 0, 0.5)",
+        height: '90%',
+        width: '90%',
+        position: 'relative',
+        left: '50%',
+        top: '50%',
+        translate: '-50% -50%',
+        textAlign: 'center',
+    }
+
+
+    return (<>
+        <div style={card}>
+            <h2>CHANCE</h2>
+            <p>Pay 20 Euro</p>
+        </div>
+    </>)
 }
 
 export default ChanceEvent;
